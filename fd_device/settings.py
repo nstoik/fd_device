f9376cd6343e7f919398c29baaa39a30ef73449f
Nelson Stoik <nelsonstoik@gmail.com>
2021-03-13 23:09:20 -0700

    Update settings.py

--- fd_device/settings.py
+++ fd_device/settings.py
@@ -60,15 +60,22 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 
-def get_config():
+def get_config(override_default=None):
+    """Return the Config option based on environment variables.
 
-    environment = os.environ.get("FD_DEVICE_CONFIG", default='dev')
+    If override_default is passed, that configuration is used instead.
+    If there is no match or nothing set then the environment defaults to 'dev'.
+    """
 
-    if environment == 'dev':
-        return DevConfig
-    elif environment == 'prod':
-        return ProdConfig
-    elif environment == 'test':
-        return TestConfig
+    if override_default is None:
+        environment = os.environ.get("FD_DEVICE_CONFIG", default="dev")
     else:
+        environment = override_default
+
+    if environment == "dev":
         return DevConfig
+    if environment == "prod":
+        return ProdConfig
+    if environment == "test":
+        return TestConfig
+    return DevConfig
