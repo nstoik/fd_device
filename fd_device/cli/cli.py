4fa2efba5f8c53f558c1dab90a1cd973c9180f8d
Nelson Stoik <nelsonstoik@gmail.com>
2021-03-24 23:23:13 -0600

    add command to start the server

--- fd_device/cli/cli.py
+++ fd_device/cli/cli.py
@@ -3,6 +3,7 @@ import click
 
 from .db import commands as db_commands
 from .manage import commands as manage_commands
+from .manage import setup_commands
 from .testing import commands as testing_commands
 
 
@@ -11,7 +12,8 @@ def entry_point():
     """Entry point for CLI."""
 
 
-entry_point.add_command(manage_commands.first_setup)
+entry_point.add_command(manage_commands.run)
+entry_point.add_command(setup_commands.first_setup)
 
 entry_point.add_command(testing_commands.test)
 entry_point.add_command(testing_commands.lint)
